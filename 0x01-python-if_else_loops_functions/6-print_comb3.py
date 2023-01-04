546c4b52e1ca16eb1ad3a7e3aaf035bafa1c4784
Bernard Allan <chwakik@gmail.com>
2023-01-04 22:34:32 +0300

    loopdeloop

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,5 +4,5 @@ for firstNum in range(0, 10):
         if firstNum == 8 and secondNum == 9:
             print("{}{}".format(firstNum, secondNum))
         else:
-            
+
             print("{}{}".format(firstNum, secondNum), end=", ")
