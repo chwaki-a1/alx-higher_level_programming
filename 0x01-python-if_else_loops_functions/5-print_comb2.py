e72109765665b634228055eba32b7bff0c6dcbff
Bernard Allan <chwakik@gmail.com>
2023-01-04 23:16:52 +0300

    loopdeloop

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i == 99:
-
-        print("{}".format(i))
-else:
-    print("{:02d}".format(i), end=", ")
+for num in range(00, 100):
+    print("{:02d}".format(num), end='\n' if num == 99 else ", ")
